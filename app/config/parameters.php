5d7edeef60f9b540948c4b31348c9deb809f86de
Artur A. Kadomsky <akadom@gmail.com>
2020-06-15 08:21:10 +0300

    0.1

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'admin_sumki_2020',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'coY3DLVETrPQ2ZW9IUSn6dFGEqz0ozaNthROvYyLqh5GOMfePvxtwEWh',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2020-06-15',
+    'locale' => 'ru-RU',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'OYBiK6fP45SGtZTD1jnxTrBEWgBwVAn9ULqs8SOek65xp7PK6iyJ0Dpe',
+    'cookie_iv' => '9C9sdc0J',
+    'new_cookie_key' => 'def00000550f2253b48ff066a6490618f2aa8e3a04a050d2038fb8d365835a124effe890c4fd478b8a02b0fcd450f7891f1539b4433cffc1d6bc2644887bcd55fc4abe8c',
+  ),
+);
---
+++ var/cache/prod/smarty/compile/33/b8/da/33b8da9a9ff908c8dd9eae46a6ef9062ee53bc57_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-06-15 08:06:30
+  from 'L:\OSPanel\domains\test.sumkispb.ru\admin234sjeoec\themes\default\template\controllers\login\layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5ee701d6e6ca47_85981356',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '33b8da9a9ff908c8dd9eae46a6ef9062ee53bc57' => 
+    array (
+      0 => 'L:\\OSPanel\\domains\\test.sumkispb.ru\\admin234sjeoec\\themes\\default\\template\\controllers\\login\\layout.tpl',
+      1 => 1592196932,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ee701d6e6ca47_85981356 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
